9de4ad37818e15e30969d66678494b4e526c9985
pranjakt <65512022+pranjakt@users.noreply.github.com>
2020-05-24 17:42:40 +0530

    Add files via upload

---
+++ module4-solutions/SpeakGoodBye.js
@@ -0,0 +1,8 @@
+var byeSpeaker = {};
+byeSpeaker.speak = function(){
+
+}
+var speakWord = "Good Bye";
+function speak(name) {
+  console.log(speakWord + " " + name);
+}
---
+++ module4-solutions/SpeakHello.js
@@ -0,0 +1,8 @@
+var helloSpeaker ={};
+helloSpeaker.speak = function(){
+
+};
+var speakWord = "Hello";
+function speak(name) {
+  console.log(speakWord + " " + name);
+}
